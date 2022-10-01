b40e832ec45b646f96121fefa82262efc3c41d4e
Owanikin <oderindeife@gmail.com>
2022-10-01 04:35:52 +0100

    project setup

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.9",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [GOERLI_PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 300000, // 300 seconds max
+    },
+}
